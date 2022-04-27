f9ddf1adccf7b5ab7ed1a9edeaaa98c09ed761ec
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-04-27 14:11:00 +0530

    yes me

--- myscript.js
+++ myscript.js
@@ -1,3 +1,13 @@
 function close_coupon() {
     document.getElementById("coupon").style.display = "none";
 }
+
+function hide_me_right() {
+    document.getElementById("scroll-nav-right").style.display = "none";
+    document.getElementById("scroll-nav-left").style.display = "inline-block";
+}
+
+function hide_me_left() {
+    document.getElementById("scroll-nav-left").style.display = "none";
+    document.getElementById("scroll-nav-right").style.display = "inline-block";
+}
