0c00a02960043ed74df2d635b38b45db205612ff
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-04-30 13:28:51 +0530

    yes me

--- myscript.js
+++ myscript.js
@@ -57,3 +57,18 @@ function change_the(x) {
     }
     document.getElementById(`pic-swap-${x}`).style.display = "block";
 }
+let day_and_night = 0;
+
+function change_mode() {
+    if (day_and_night % 2 == 0) {
+        document.getElementById('change-mode').classList.remove('bi-brightness-high-fill');
+        document.getElementById('change-mode').classList.add('bi-moon-stars-fill');
+        document.body.style.backgroundColor = "grey";
+        day_and_night++;
+    } else {
+        document.getElementById('change-mode').classList.remove('bi-moon-stars-fill');
+        document.getElementById('change-mode').classList.add('bi-brightness-high-fill');
+        document.body.style.backgroundColor = "rgb(207, 210, 213)";
+        day_and_night--;
+    }
+}
